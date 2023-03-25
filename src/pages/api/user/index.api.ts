1e5d2a001797c8b065e5e8bf72c26c7d6ff2997e
Arison Reis <arisontechp@gmail.com>
2023-03-25 16:45:59 -0300

    fixing the cookies error

--- src/pages/api/user/index.api.ts
+++ src/pages/api/user/index.api.ts
@@ -10,10 +10,19 @@ interface IDataUser {
 export default function handleUser(req: NextApiRequest, res: NextApiResponse) {
   const { name, email, perfilUrl }: IDataUser = req.body;
 
-  setCookie({ res }, 'webchat:UserName', name.toLowerCase(), {
-    maxAge: 60 * 60 * 24 * 2, //2 dias
-    path: '/',
-  });
+  if (!name && !email && !perfilUrl) {
+    return res.status(404).json({
+      status: 'error',
+      message: ' all data is invalid',
+    });
+  }
+
+  if (name) {
+    setCookie({ res }, 'webchat:UserName', name.toLowerCase(), {
+      maxAge: 60 * 60 * 24 * 2, //2 dias
+      path: '/',
+    });
+  }
 
   if (email) {
     setCookie({ res }, 'webchat:Email', email.toLowerCase(), {
