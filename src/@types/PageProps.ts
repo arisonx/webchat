ea2ecd1c8d98ce9227f0a7eee7bd8be46d8d0d55
Arison Reis <arisonreis157@gmail.com>
2023-05-07 00:16:35 -0300

    🔒 chore(PageProps.ts): adding session cookie and user typings

--- src/@types/PageProps.ts
+++ src/@types/PageProps.ts
@@ -3,5 +3,12 @@ export interface IPageProps {
     'webchat:name'?: string;
     'webchat:perfilurl'?: string;
     'webchat:email'?: string;
+    'session' ?: string;
+  };
+  user?: {
+    id: string;
+    name: string;
+    email: string;
+    perfil_url: string;
   };
 }
